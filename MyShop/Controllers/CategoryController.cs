cc00846444af35da0c79b235bdc171e8d038037e
DESKTOP-98NGS9V\Александр <oshino.shinobu@yandex.ru>
2023-02-10 22:11:30 +0300

    Added validation for Category, both from the client side and from the server side

--- MyShop/Controllers/CategoryController.cs
+++ MyShop/Controllers/CategoryController.cs
@@ -26,9 +26,14 @@ namespace MyShop.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
         }
     }
 }
--- MyShop/Models/Category.cs
+++ MyShop/Models/Category.cs
@@ -1,9 +1,14 @@
+using Microsoft.Build.Framework;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 public class Category
 {
     public int Id { get; set; }
+    [System.ComponentModel.DataAnnotations.Required]
     public string? Name { get; set; }
     [DisplayName("Display Order")]
+    [System.ComponentModel.DataAnnotations.Required]
+    [Range(1,int.MaxValue,ErrorMessage = "Display Order for the category must be greather then 0")]
     public int DisplayOrder { get; set; }
 }
