9439dd2ac6bda670551f751bb07764ec05aa73b7
DESKTOP-98NGS9V\Александр <oshino.shinobu@yandex.ru>
2023-02-11 00:12:10 +0300

    Add CRUD operations to Category. Add Awesome fonts to app and changed some icons

--- MyShop/Controllers/CategoryController.cs
+++ MyShop/Controllers/CategoryController.cs
@@ -32,8 +32,64 @@ namespace MyShop.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            return View(obj);            
+        }
+
+        //Get edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
             return View(obj);
-            
+        }
+        //Post edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //Get delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //Post delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+                _db.Category.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
         }
     }
 }
